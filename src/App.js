4ffa6af36a2370dfa3a517e1480a2d32b02c6cc7
Mitsudani <mdsachetti@gmail.com>
2021-11-29 23:24:00 -0300

    App finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
+import ListadoNoticias from "./components/ListadoNoticias";
 
 function App() {
+  // Definir la categoía y noticias
+  const [categoria, guardarCategoria] = useState("");
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://gnews.io/api/v4/top-headlines?&country=en&topic=${categoria}&token=43dcb415b37cb19727ecfb5b314442f0`;
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+      guardarNoticias(noticias.articles);
+    };
+    consultarAPI();
+  }, [categoria]);
+
   return (
     <Fragment>
       <Header titulo="Buscado de Noticias" />
       <div className="container white">
-        <Formulario />
+        <Formulario guardarCategoria={guardarCategoria} />
+        <ListadoNoticias noticias={noticias} />
       </div>
     </Fragment>
   );
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,12 +1,34 @@
 import React from "react";
 import styles from "./Formulario.module.css";
+import useSelect from "../hooks/useSelect";
+import PropTypes from "prop-types";
 
-const Formulario = () => {
+const Formulario = ({ guardarCategoria }) => {
+  const Opciones = [
+    { value: "general", label: "General" },
+    { value: "business", label: "Negocios" },
+    { value: "entertainment", label: "Entretenimiento" },
+    { value: "health", label: "Salud" },
+    { value: "science", label: "Ciencia" },
+    { value: "sports", label: "Deportes" },
+    { value: "technology", label: "Tecnología" },
+  ];
+
+  // Utilizar custom hook
+  const [categoria, SelectNoticias] = useSelect("general", Opciones);
+
+  // Sumbit formulario, pasa categoría a App.js
+  const buscarNoticias = (e) => {
+    e.preventDefault();
+    guardarCategoria(categoria);
+  };
+
   return (
     <div className={`${styles.buscador} row`}>
       <div className="col s12 m8 offset-m2">
-        <form>
+        <form onSubmit={buscarNoticias}>
           <h2 className={styles.heading}>Encuentra noticias por categoría</h2>
+          <SelectNoticias />
           <div className="input-field col s12">
             <input
               type="submit"
@@ -18,6 +40,10 @@ const Formulario = () => {
       </div>
     </div>
   );
+};
+
+Formulario.propTypes = {
+  guardarCategoria: PropTypes.func.isRequired,
 };
 
 export default Formulario;
